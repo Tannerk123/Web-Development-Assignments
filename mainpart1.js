d2b653627b7e920e54f1850b0dd211683bd3e34e
Tannerk123 <156024826+Tannerk123@users.noreply.github.com>
2024-03-25 16:21:05 -0400

    Assignment-4 Part-1
    Finished result() function

--- mainpart1.js
+++ mainpart1.js
@@ -20,18 +20,24 @@ function result() {
   let yItem = randomValueFromArray(insertY)
   let zItem = randomValueFromArray(insertZ)
 
+  newStory = newStory.replaceAll(':insertx:', xItem)
+  newStory = newStory.replaceAll(':inserty:', yItem)
+  newStory = newStory.replaceAll(':insertz:', zItem)
+
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name)
 
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = Math.round(300*0.0714286) + ' stone';
+    const temperature =  Math.round((94-32)*5/9) + ' Celsius';
+    newStory = newStory.replaceAll('94 fahrenheit', temperature)
+    newStory = newStory.replaceAll('300 pounds', weight)
   }
 
-  story.textContent = 
+  story.textContent = newStory
   story.style.visibility = 'visible';
 }
